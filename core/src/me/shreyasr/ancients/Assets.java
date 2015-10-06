586efeddd55c89c1e2bc0cd9e7030f44511c5860
Shreyas <skraman1999@gmail.com>
2015-10-06 12:22:36 -0700

    Basic asset loading.

---
+++ core/src/me/shreyasr/ancients/Assets.java
@@ -0,0 +1,11 @@
+package me.shreyasr.ancients;
+
+import com.badlogic.gdx.graphics.Texture;
+import net.dermetfan.gdx.assets.AnnotationAssetManager;
+
+@SuppressWarnings("SpellCheckingInspection")
+public class Assets {
+
+    @AnnotationAssetManager.Asset(Texture.class)
+    public static final String BADLOGIC = "badlogic.jpg";
+}
--- core/src/me/shreyasr/ancients/Game.java
+++ core/src/me/shreyasr/ancients/Game.java
@@ -5,16 +5,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import net.dermetfan.gdx.assets.AnnotationAssetManager;
 
 public class Game extends ApplicationAdapter {
 
     SpriteBatch batch;
-    Texture img;
+    AnnotationAssetManager assetManager;
 
     @Override
     public void create() {
+        assetManager = new AnnotationAssetManager();
+        assetManager.load(Assets.class);
+
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
     }
 
     @Override
@@ -22,7 +25,7 @@ public class Game extends ApplicationAdapter {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0, 0);
+        batch.draw(assetManager.get(Assets.BADLOGIC, Texture.class), 0, 0);
         batch.end();
     }
 }
